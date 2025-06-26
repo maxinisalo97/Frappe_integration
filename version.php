09219f3ae526c418d6b73e94704f96dde250f359
Maximiliano <maxinisalo97@gmail.com>
2025-06-26 10:11:40 +0000

    Update TimeZone

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_frappe_integration';
-$plugin->release = 'v0.0.115';
+$plugin->release = 'v0.0.116';
 $plugin->version   = 2025062211;   // YYYYMMDDXX
 $plugin->requires  = 2018051700;   
 $plugin->maturity  = MATURITY_ALPHA;
