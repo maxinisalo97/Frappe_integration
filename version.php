a16822b09dd860deba3c294c7c013aa4a4508256
Maximiliano <maxinisalo97@gmail.com>
2025-07-09 09:39:41 +0000

    Change Order Items Grade

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_frappe_integration';
-$plugin->release = 'v0.0.135';
-$plugin->version   = 20250622208;   // YYYYMMDDXX
+$plugin->release = 'v0.0.136';
+$plugin->version   = 20250622210;   // YYYYMMDDXX
 $plugin->requires  = 2018051700;   
 $plugin->maturity  = MATURITY_ALPHA;
